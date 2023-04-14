71abd242e4fc96f62e07ce7c171fa418fb857cd3
Huỳnh Văn Toàn <huynhvantoan.itc@gmail.com>
2023-04-14 12:13:01 +0700

    Update MyFirebaseMessagingService.kt

--- app/src/main/java/com/netacom/vndirect/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/netacom/vndirect/MyFirebaseMessagingService.kt
@@ -1,6 +1,6 @@
 package com.netacom.vndirect
 
-import com.asia.sdkbase.logger.Logger
+import android.util.Log
 import com.asia.sdkui.ui.sdk.NetAloSDK
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -9,16 +9,16 @@ import org.json.JSONObject
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Logger.e("onNewToken::$token")
+        Log.e("onNewToken::", token)
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        Logger.e("MyFirebaseMessagingService::${message.data}")
+        Log.e("MyFirebaseMessagingService::", message.data.toString())
         val jsonPayload = JSONObject(message.data.toString()).getJSONObject("Data").getString("Payload")
         val jsonObject = JSONObject(jsonPayload)
-        Logger.e("jsonPayload::$jsonPayload")
-        Logger.e("jsonObject::$jsonObject")
+        Log.e("jsonPayload::", jsonPayload)
+        Log.e("jsonObject::", jsonObject.toString())
         if (jsonObject.has("call")) {
             //Send notification call
             NetAloSDK.onFirebaseReceivedCall(applicationContext, jsonPayload)
